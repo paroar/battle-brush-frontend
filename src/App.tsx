12b20794eb5b9c61e4bc851a60e222e09a9eb863
paroar <prodriguezarmida@gmail.com>
2021-03-08 18:38:41 +0100

    Add Sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import './../node_modules/antd/dist/antd.css'
 import { WSContextProvider } from './contexts/websocket';
 import Home from './pages/home/home';
