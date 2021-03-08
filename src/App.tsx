72df73a0c1409081592f9ec6036973caab12c431
paroar <prodriguezarmida@gmail.com>
2021-03-08 19:29:01 +0100

    Clean App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.scss';
-import './../node_modules/antd/dist/antd.css'
 import { WSContextProvider } from './contexts/websocket';
 import Home from './pages/home/home';
 
