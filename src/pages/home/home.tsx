ce8a1e953aa3e19a232362f428903156f56e2067
paroar <prodriguezarmida@gmail.com>
2021-03-12 23:38:48 +0100

    Vivus home animation svg

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,20 +1,20 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useLayoutEffect } from 'react'
 import ChoiceRoom from '../../components/ChoiceRoom/ChoiceRoom'
 import { WSContext } from '../../contexts/websocket'
 import Lobby from '../../components/Lobby/Lobby'
 import main from '../../assets/main.svg'
-import ReactVivus from 'react-vivus';
 import Vivus from 'vivus'
+
 
 const Home = () => {
 
     const { room } = useContext(WSContext)
 
-    useEffect(() => {
-        new Vivus('my-div', { 
-            duration: 1500, 
+    useLayoutEffect(() => {
+        new Vivus('my-div', {
+            duration: 1000,
             file: main,
-            type: "oneByOne"
+            type: 'oneByOne',
         })
     }, [])
 
@@ -22,12 +22,14 @@ const Home = () => {
         return <Lobby />
     } else {
         return (
-            <section className="home">
-                <h1 className="title">what the f*** is that</h1>
-                <div id="my-div" className="svg"></div>
-                <ChoiceRoom />
-                <span className="contact">Want to add your drawing to the front page? Contact me at <a href="">email</a></span>
-            </section>
+            <>
+                <section className="home">
+                    <h1 className="title">what the f*** is that</h1>
+                    <div id="my-div" className="svg"></div>
+                    <ChoiceRoom />
+                    <span className="contact">Want to add your drawing to the front page? Contact me at <a href="">email</a></span>
+                </section>
+            </>
         )
     }
 
